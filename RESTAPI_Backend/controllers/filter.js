ea5fd89b6cc9006a25cfc6c4849c18ff0dbe2982
deepak gupta <dg91918@gmail.com>
2024-07-19 12:45:10 +0530

    loading on search

--- RESTAPI_Backend/controllers/filter.js
+++ RESTAPI_Backend/controllers/filter.js
@@ -6,17 +6,20 @@ const getItemByName = async (req, res) => {
   try {
     // console.log(req);
     const {name} = req.body;
-    const products = await Product.find({ });
-    // console.log(products.length, name);
-    const matches = stringSimilarity.findBestMatch(name.toLowerCase(), products.map(p => p.name.toLowerCase()));
+    // const products = await Product.find({ });
+    // // console.log(products.length, name);
+    // const matches = stringSimilarity.findBestMatch(name.toLowerCase(), products.map(p => p.name.toLowerCase()));
 
-    let filteredProduct = [];
-    matches.ratings.forEach((curElem,index)=>{
-        if(curElem.rating>=0.4){
-            filteredProduct.push(products[index]);
-        }
-    })
+    // let filteredProduct = [];
+    // matches.ratings.forEach((curElem,index)=>{
+    //     if(curElem.rating>=0.45){
+    //         filteredProduct.push(products[index]);
+    //     }
+    // })
 
+    const regex = new RegExp(name, 'i');
+    const filteredProduct = await Product.find({ name: regex }).exec();
+    console.log(filteredProduct)
     res.status(200).json({ "products": filteredProduct });
     
   } catch(error) {
